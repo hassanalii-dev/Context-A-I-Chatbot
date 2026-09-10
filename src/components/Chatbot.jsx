7e291cb051958c97d4168e24663280708d4e689f
Hassan Ali <hassanmughal085870@gmail.com>
2026-09-10 14:33:49 +0500

    Chatbot

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -4,7 +4,7 @@ import { UserProvider } from "../context/UserContext";
 import "./Chatbot.css";
 
 const ai = new GoogleGenAI({
-  apiKey: import.meta.env.VITE_GEMINI_API_KEY,
+  apiKey: "AQ.Ab8RN6Jtf8GafqKH0nY7hEicPEf8PmlVPz27ixtz3rjvtD16Kg",
 });
 
 function Chatbot() {
